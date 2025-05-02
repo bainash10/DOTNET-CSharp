29a49d947ed6a4ffbe852f43e1b5f45aeb08d5c9
Bainash <nischalbaidar@gmail.com>
2025-05-02 08:48:55 +0545

    done

---
+++ ! Assignments and Practice Questions in class/Inheritance Form/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Inheritance_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            label1 = new Label();
+            panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(154, 0);
+            splitContainer1.Margin = new Padding(2, 1, 2, 1);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.PaleGreen;
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint_1;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint;
+            splitContainer1.Size = new Size(468, 387);
+            splitContainer1.SplitterDistance = 72;
+            splitContainer1.SplitterWidth = 2;
+            splitContainer1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Info;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label1.Location = new Point(185, 25);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 25);
+            label1.TabIndex = 0;
+            label1.Text = "FORM";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.PaleGreen;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button3);
+            panel1.Location = new Point(-1, 0);
+            panel1.Margin = new Padding(2, 1, 2, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(157, 390);
+            panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 40);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 48);
+            button1.TabIndex = 55;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(27, 100);
+            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 46);
+            button2.TabIndex = 56;
+            button2.Text = "Register";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(27, 157);
+            button3.Margin = new Padding(2, 1, 2, 1);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 46);
+            button3.TabIndex = 57;
+            button3.Text = "Calculator";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(620, 351);
+            Controls.Add(panel1);
+            Controls.Add(splitContainer1);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Panel panel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+    }
+}
---
+++ ! Assignments and Practice Questions in class/Inheritance Form/Form1.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Windows.Forms;
+
+namespace Inheritance_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFormIntoPanel(Form form)
+        {
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Clear();
+            splitContainer1.Panel2.Controls.Add(form);
+            form.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login loginPage = new Login();
+            LoadFormIntoPanel(loginPage);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Register registerPage = new Register();
+            LoadFormIntoPanel(registerPage);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Calculator calculatorPage = new Calculator();
+            LoadFormIntoPanel(calculatorPage);
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e) { }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e) { }
+
+        private void splitContainer1_Panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
